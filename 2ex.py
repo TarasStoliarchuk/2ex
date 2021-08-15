c889fb8d0f50e2bbbc4fb3d7d51084ea8e51586a
TarasStoliarchuk <86238977+TarasStoliarchuk@users.noreply.github.com>
2021-08-15 05:32:04 +0300

    first commit

---
+++ 2ex.py
@@ -0,0 +1,10 @@
+ls = [i for i in range(11, 80)]
+for i in ls:
+    if i % 3 == 0 and i % 5 == 0:
+        print('$$@@')
+    elif i % 3 == 0:
+        print('$$')
+    elif i % 5 == 0:
+        print('@@')
+    else:
+        print(i)
